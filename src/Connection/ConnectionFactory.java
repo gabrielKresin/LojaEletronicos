bae4dc420982fb196f4e481ba9f663a22eaaee33
gabriel kresin <kresingabriel@gmail.com>
2018-06-26 10:30:18 -0300

    feito a connection factory

--- src/Connection/ConnectionFactory.java
+++ src/Connection/ConnectionFactory.java
@@ -1,5 +1,28 @@
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+
+    private final String url = "jdbc:mysql://localhost:3306/loja";
+    private final String user = "root";
+    private final String password = "";
+
+    public Connection obterConexao() {
+
+        Connection conexao = null;
+
+        try {
+            conexao = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão realizada com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao realizar conexão -> "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        //Retornar a conexão
+        return conexao;
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,13 @@
 package Main;
 
+import Connection.ConnectionFactory;
+
 public class Main {
     
     public static void main(String[] args) {
+        
+        ConnectionFactory cf = new ConnectionFactory();
+        cf.obterConexao();
         
     }
 }
